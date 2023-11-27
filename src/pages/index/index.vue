c59578f188aecdaa55dbbdbd66f83cec8fd546a0
御琼 <2824019551@qq.com>
2023-11-27 11:20:46 +0800

    feat: 前台分类组件导入

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,6 +2,7 @@
 //
 import XtxSwiper from '@/components/XtxSwiper.vue'
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { getHomeBannerApi } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -23,9 +24,13 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel />
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+//调整页面底色
+page {
+  background-color: #f7f7f7;
+}
 </style>
