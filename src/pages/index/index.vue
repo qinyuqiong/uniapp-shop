c32033f1a56ffdb1cbb0db1ed91d8fb3b90e9b89
御琼 <2824019551@qq.com>
2023-11-28 10:53:30 +0800

    feat:猜你喜欢组件+不滚动首行栏

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -42,18 +42,31 @@ onLoad(() => {
 <template>
   <!-- 自定义导航栏 -->
   <CustomNavbar />
-  <!-- 自定义轮播图 -->
-  <XtxSwiper :list="bannerList" />
-  <!-- 分类面板 -->
-  <CategoryPanel :list="categoryList" />
-  <!-- 热门推荐 -->
-  <HotPanel :list="hotList"></HotPanel>
-  <view class="index">index</view>
+  <scroll-view class="scroll-view" scroll-y>
+    <!-- 自定义轮播图 -->
+    <XtxSwiper :list="bannerList" />
+    <!-- 分类面板 -->
+    <CategoryPanel :list="categoryList" />
+    <!-- 热门推荐 -->
+    <HotPanel :list="hotList"></HotPanel>
+    <!-- 猜你喜欢 -->
+    <XtxGuess></XtxGuess>
+    <view class="index">index</view>
+  </scroll-view>
 </template>
 
 <style lang="scss">
 //调整页面底色
 page {
   background-color: #f7f7f7;
+  // 设置页面高度
+  height: 100%;
+  // 设置页面最小宽度
+  display: flex;
+  // 设置页面主轴方向为竖向
+  flex-direction: column;
+}
+.scroll-view {
+  flex: 1;
 }
 </style>
