0505bf5c3a69151f043f27e5cea388cebc1bd4a6
御琼 <2824019551@qq.com>
2023-12-12 17:13:21 +0800

    商品详情-轮播图交互

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -19,6 +19,21 @@ const getGoodsByIdData = async () => {
   goods.value = res.result
 }
 
+//轮播图变化
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail.current
+}
+
+//点击图片
+const onTapImage = (url: string) => {
+  //大图预览
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
+
 onLoad(() => {
   getGoodsByIdData()
 })
@@ -30,15 +45,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper @change="onChange" circular>
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onTapImage(item)" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
