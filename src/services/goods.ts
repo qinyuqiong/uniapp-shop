9e1b57e569a1c7a9cb33d46493fc98d852f3f03f
御琼 <2824019551@qq.com>
2023-12-12 16:58:51 +0800

    商品详情-页面渲染

--- src/services/goods.ts
+++ src/services/goods.ts
@@ -1,7 +1,8 @@
+import type { GoodsResult } from '@/types/goods'
 import { http } from '@/utils/http'
 
 export const getGoodsByIdApi = (id: string) => {
-  return http({
+  return http<GoodsResult>({
     method: 'GET',
     url: '/goods',
     data: {
