9a7efd76de01e1835cfb1de350a16227f48a7e18
御琼 <2824019551@qq.com>
2023-12-19 09:33:24 +0800

    地址列表渲染

--- src/services/address.ts
+++ src/services/address.ts
@@ -1,4 +1,4 @@
-import type { AddressParams } from '@/types/addresss'
+import type { AddressItem, AddressParams } from '@/types/addresss'
 import { http } from '@/utils/http'
 
 /**
@@ -11,5 +11,15 @@ export const postMemberAddressAPI = (data: AddressParams) => {
     method: 'POST',
     url: '/member/address',
     data,
+  })
+}
+
+/**
+ * 获取收货地址列表
+ */
+export const getMemberAddressAPI = () => {
+  return http<AddressItem[]>({
+    method: 'GET',
+    url: '/member/address',
   })
 }
